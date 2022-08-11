90a238da8c74d6f30a2be1854d9ade2335240893
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-10 19:48:27 -0500

    finished studio

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dictionary<char, int> characters = new Dictionary<char, int>();
+
+string test = "hello";
+
+foreach (char letter in test)
+{
+    int count = 0;
+
+    if (!characters.ContainsKey(letter))
+    {
+        count++;
+        characters.Add(letter, count);
+    }
+    else
+    {
+        characters[letter] = characters[letter] + 1;
+    }
+}
+
+foreach (KeyValuePair<char, int> character in characters)
+{
+    Console.WriteLine(character.Key + ": " + character.Value);
+}
